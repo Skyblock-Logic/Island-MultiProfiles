16066c4dde5daf3fcc223fbc1e4137a50e407db2
NewJKH <newsun0805@gmail.com>
2026-01-20 16:03:44 +0900

    feat: 이벤트 및 커맨드 리스너 등록 로직 추가
    - IslandService 초기화 및 관리
    - IMCmdSubIslandCreate, IMCmdTeleport 커맨드 등록
    - IslandUpdateListener 이벤트 리스너 등록

--- src/main/java/com/nano/islandMultiProfiles/IslandMultiProfiles.java
+++ src/main/java/com/nano/islandMultiProfiles/IslandMultiProfiles.java
@@ -2,16 +2,34 @@ package com.nano.islandMultiProfiles;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.bgsoftware.superiorskyblock.api.SuperiorSkyblockAPI;
+import com.nano.islandMultiProfiles.infra.command.IMCmdSubIslandCreate;
+import com.nano.islandMultiProfiles.infra.command.IMCmdTeleport;
+import com.nano.islandMultiProfiles.listener.IslandUpdateListener;
+import com.nano.islandMultiProfiles.service.IslandService;
+
 public final class IslandMultiProfiles extends JavaPlugin {
+
+    private final IslandService islandService = new IslandService(this);
+
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        onCommand();
+        onEvent();
+    }
 
+    private void onCommand(){
+        SuperiorSkyblockAPI.getCommands().registerCommand(new IMCmdSubIslandCreate(islandService));
+        SuperiorSkyblockAPI.getCommands().registerCommand(new IMCmdTeleport(islandService));
+    }
+
+    private void onEvent(){
+        getServer().getPluginManager().registerEvents(new IslandUpdateListener(islandService),this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
