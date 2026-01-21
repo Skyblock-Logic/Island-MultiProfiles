c36c20cbad0634394baeedf75d684cd80699a3f8
NewJKH <newsun0805@gmail.com>
2026-01-21 10:38:13 +0900

    refactor: 필요없는 엔티티 삭제 (IslandProfile, Group, GroupPlayer)

--- src/main/java/com/nano/islandMultiProfiles/entity/player/Group.java
+++
@@ -1,5 +0,0 @@
-package com.nano.islandMultiProfiles.entity.player;
-
-import java.util.UUID;
-
-public record Group(long groupId, UUID owner) {}
--- src/main/java/com/nano/islandMultiProfiles/entity/player/GroupPlayer.java
+++
@@ -1,5 +0,0 @@
-package com.nano.islandMultiProfiles.entity.player;
-
-import java.util.UUID;
-
-public record GroupPlayer(UUID playerUUID, long groupId, int roleId) { }
