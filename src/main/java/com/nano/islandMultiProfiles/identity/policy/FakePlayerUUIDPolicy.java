a53676e8dd115b8bec80652a95b3e7a3302c2855
NewJKH <newsun0805@gmail.com>
2026-01-29 13:11:24 +0900

    refactor: `issue` 메서드 불필요한 매개변수 제거 및 내부 로직 단순화

--- src/main/java/com/nano/islandMultiProfiles/identity/policy/FakePlayerUUIDPolicy.java
+++ src/main/java/com/nano/islandMultiProfiles/identity/policy/FakePlayerUUIDPolicy.java
@@ -6,8 +6,8 @@ import java.util.UUID;
 public final class FakePlayerUUIDPolicy {
 	private FakePlayerUUIDPolicy() {}
 
-	public static UUID issue(UUID playerUUID, int slot){
-		String source = "playerUUID:" + playerUUID.toString() + ",group:" + slot;
+	public static UUID issue(UUID playerUUID){
+		String source = "playerUUID:" + playerUUID.toString();
 		return UUID.nameUUIDFromBytes(source.getBytes(StandardCharsets.UTF_8));
 	}
 }
